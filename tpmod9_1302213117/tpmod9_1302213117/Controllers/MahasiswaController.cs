6ce4c39c4a95d1fba230e308fa02203a10954635
Regyyyy <regyrahman@gmail.com>
2023-04-17 07:49:20 +0700

    menambahkan API mahasiswa
    Program selesai

---
+++ tpmod9_1302213117/tpmod9_1302213117/Controllers/MahasiswaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tpmod9_1302213117.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        static List<Mahasiswa> dataUsers = new List<Mahasiswa>
+        {
+            new Mahasiswa("Alif Taufiqurrahman","1302213011"),
+            new Mahasiswa("Deva Angela Felisitas Toding","1302213011"),
+            new Mahasiswa("Iqro Banyuanto","1302213061"),
+            new Mahasiswa("Rahma Sakti Rahardian","1302210095"),
+            new Mahasiswa("Regy Renanda Rahman","1302213117")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataUsers;
+        }
+
+        [HttpGet("(id")]
+        public Mahasiswa Get(int id)
+        {
+            return dataUsers[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa newMhs)
+        {
+            dataUsers.Add(newMhs);
+        }
+
+        [HttpPut("(id)")]
+        public void Put(int id, [FromBody]Mahasiswa updatedMhs)
+        {
+            dataUsers[id] = updatedMhs;
+        }
+
+        [HttpDelete("(id)")]
+        public void Delete(int id)
+        {
+            dataUsers.RemoveAt(id);
+        }
+    }
+}
---
+++ tpmod9_1302213117/tpmod9_1302213117/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tpmod9_1302213117
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public Mahasiswa() { }
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+    }
+}
